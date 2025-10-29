3d77be72a12c20fc8ce1deaf9d7d07856f7b4d30
hjgode <hjgode@gmail.com>
2025-10-29 09:54:53 +0100

    added shell background launchers for uniclip client and server

---
+++ uniclip.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+PIDFILE=/var/lock/uniclip.pid
+remove_pidfile()
+{
+  rm -f "$PIDFILE"
+}
+another_instance()
+{
+  echo "run uniclip 192.168.0.40:53701 on client"
+  echo "There is another instance running, exiting"
+  exit 1
+}
+if [ -f "$PIDFILE" ]; then
+  kill -0 "$(cat $PIDFILE)" && another_instance
+fi
+trap remove_pidfile EXIT
+
+echo $$ > "$PIDFILE"
+
+#if [ "$(pgrep uniclip)" != $$ ]; then #$$ = the process ID of the current shell.
+if pgrep -x "uniclip" > /dev/null
+then
+     another_instance
+fi
+
+cd /home/hgode/bin/uniclip/
+./uniclip -d -p 53701 &
+echo "run uniclip 192.168.0.40:53701 on client"
+
